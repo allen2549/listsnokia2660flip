19b7acfd06690b2d6168873b2f3a301d6165c978
Alessandro Ribeiro <60798481+allen2549@users.noreply.github.com>
2023-11-21 10:39:04 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,63 +1,44 @@
-var contador = 1;
+document.addEventListener("DOMContentLoaded", function() {
+    carregarListaTarefas();
+});
+
+function carregarListaTarefas() {
+    var lista = document.getElementById("lista");
+
+    while (lista.firstChild) {
+        lista.removeChild(lista.firstChild);
+    }
+
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            var listaTarefas = JSON.parse(xhr.responseText);
+
+            listaTarefas.forEach(function(tarefa) {
+                var novoItem = document.createElement("li");
+                novoItem.appendChild(document.createTextNode(tarefa));
+                lista.appendChild(novoItem);
+            });
+        }
+    };
+
+    xhr.open("GET", "./interacao_bd.php", true);
+    xhr.send();
+}
 
 function adicionarItem() {
-  var descricaoInput = document.getElementById("descricao").value;
-  if (descricaoInput.trim() !== "") {
-    var lista = document.getElementById("lista");
-    var novoItem = document.createElement("li");
-    novoItem.id = "item" + contador;
-
-    var concluirButton = document.createElement("button");
-    concluirButton.className = "concluir";
-    concluirButton.innerHTML = ">";
-    concluirButton.onclick = function() {
-      concluirItem(novoItem.id);
-    };
-
-    var excluirButton = document.createElement("button");
-    excluirButton.className = "excluir";
-    excluirButton.innerHTML = "X";
-    excluirButton.onclick = function() {
-      excluirItem(novoItem.id);
-    };
-
-    novoItem.appendChild(document.createTextNode(descricaoInput));
-    novoItem.appendChild(concluirButton);
-    novoItem.appendChild(excluirButton);
-    lista.appendChild(novoItem);
-    document.getElementById("descricao").value = "";
-    contador++;
-
-    // Aqui você pode adicionar código para enviar os dados para o servidor (opcional).
-
+    // Função adicionarItem aqui
     return false;
-  }
-  return false;
 }
 
 function concluirItem(id) {
-  var item = document.getElementById(id);
-  var marcado = item.style.textDecoration === "line-through";
-
-  if (marcado) {
-    item.style.textDecoration = "none";
-    item.style.color = "white";
-  } else {
-    item.style.textDecoration = "line-through";
-    item.style.color = "green";
-  }
+    // Função concluirItem aqui
 }
 
 function excluirItem(id) {
-  var item = document.getElementById(id);
-  item.parentNode.removeChild(item);
+    // Função excluirItem aqui
 }
 
 function limpar() {
-  var lista = document.getElementById("lista");
-  var ultimoItem = lista.lastElementChild;
-
-  if (ultimoItem) {
-    lista.removeChild(ultimoItem);
-  }
+    // Função limpar aqui
 }
