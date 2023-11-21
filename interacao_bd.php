10b05bc43ca3e0c05570b538d721c2b7a1ed5528
Alessandro Ribeiro <60798481+allen2549@users.noreply.github.com>
2023-11-21 10:39:31 -0300

    Update interacao_bd.php

--- interacao_bd.php
+++ interacao_bd.php
@@ -11,19 +11,21 @@ if (!$conn) {
     die("Falha na conexão com o banco de dados");
 }
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    if (isset($_POST["acao"]) && $_POST["acao"] == "adicionarItem") {
-        $descricao = $_POST["descricao"];
-        $query = "INSERT INTO listadetarefas (descricao) VALUES ($1)";
-        $result = pg_query_params($conn, $query, array($descricao));
+// Consulta para obter todas as tarefas da tabela "listadetarefas"
+$query = "SELECT * FROM listadetarefas";
+$result = pg_query($conn, $query);
 
-        if ($result) {
-            echo json_encode(array("status" => "success", "mensagem" => "Item adicionado com sucesso."));
-        } else {
-            echo json_encode(array("status" => "error", "mensagem" => "Erro ao adicionar o item."));
-        }
-    }
+if (!$result) {
+    die("Erro na consulta ao banco de dados");
 }
+
+// Monta a lista de tarefas em formato JSON para ser lida pelo JavaScript
+$listaTarefas = array();
+while ($row = pg_fetch_assoc($result)) {
+    $listaTarefas[] = $row['descricao'];
+}
+
+echo json_encode($listaTarefas);
 
 pg_close($conn);
 ?>
